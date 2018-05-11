21dfcffddbc1204996a5e2e7ec4ad29ba9215378
JJSlabbert <38969599+JJSlabbert@users.noreply.github.com>
2018-05-11 17:09:15 +0200

    Update SDP810_example.py

--- SDP810_example.py
+++ SDP810_example.py
@@ -42,9 +42,9 @@ for x in range (0, 5):
     pressure_value=reading[0]+float(reading[1])/255
     if pressure_value>=0 and pressure_value<128:
         diffirential_pressure=pressure_value*240/256 #scale factor adjustment
-    elif pressure_value>128 and pressure_value<256:
+    elif pressure_value>128 and pressure_value<=256:
         diffirential_pressure=-(256-pressure_value)*240/256 #scale factor adjustment
-    elif pressure_value==256 or pressure_value==128:
+    elif pressure_value==128:
         diffirential_pressure=99999999 #Out of range
     print("Diffirential Pressure: "+str(diffirential_pressure)+" PA")
 
