c80c159d7fc43d7dcfe1949cddf2bb168f92bb56
JJSlabbert <38969599+JJSlabbert@users.noreply.github.com>
2018-12-21 11:37:55 +0200

    Update 2-x-sdp810.py

--- 2-x-sdp810.py
+++ 2-x-sdp810.py
@@ -1,5 +1,5 @@
 #IMPORTANT: Add the following 2 lines to /boot/config.txt to generate aditional i2c busses 3 and 4
-#dtoverlay=i2c-gpio,bus=3,i2c_gpio_delay_us=1
+#dtoverlay=i2c-gpio,bus=3,i2c_gpio_delay_us=1,i2c_gpio_sda=23,i2c_gpio_scl=24
 #dtoverlay=i2c-gpio,bus=4,i2c_gpio_delay_us=1,i2c_gpio_sda=17,i2c_gpio_scl=27
 #Code should be compatable with sdp810, sdp800 sdp811 and sdp801
 #Dev by JJ Slabbert
